541c68c603b49eacb0cc56e92a2cfad0be1be5e0
natashapinto <natasha.b.pinto@gmail.com>
2022-01-31 13:32:19 +0000

    add bcftools

--- Readme.md
+++ Readme.md
@@ -1,7 +1,8 @@
-# DNAnexus FH PRS v1.0
+# DNAnexus FH PRS v1.0.0
 
 ## What does this app do?
-This app calculates a PRS based on an input VCF. It uses the dockerised code from [https://github.com/moka-guys/fhprs].
+The app takes in a compressed GVCF and uses bcftools convert to change it to a uncompressed VCF based on a provided BED file.
+The output uncompressed VCF is used as an input to calculate PRS. It uses the dockerised code from [https://github.com/moka-guys/fhprs].
 
 ## What are typical use cases for this app?
 It is used to calculate the polygenic risk score (PRS) for FH samples.
@@ -9,19 +10,21 @@ It is used to calculate the polygenic risk score (PRS) for FH samples.
 ## What inputs are required for this app to run?
 This app requires the following inputs:
 
-- g.vcf (compressed or uncompressed)
+- g.vcf (compressed)
+- BEDfile containing regions of interest
+- Reference Genome in a tar file
 - skip (boolean flag - true (default) or false). If skip = true the app runs but finishes without any data processing. This is so we can embed the app in a generic workflow that would run more than just FH samples.
 
-
 ## How does this app work?
-If skip is not true the input VCF is downloaded, and if required, decompressed using gunzip.
+If skip is not true the input GVCF is downloaded, and decompressed and compressed as a bgzip file (requirment for bcftools).
+Bcftools then converts GVCF to a VCF, in a format required by the FH dockerised image. The output VCF is saved in PRS_output/
 The samplename is captured from the vcf file name (everything before the first full stop).
 The docker image packaged in the app is loaded and run, with the output written to a subfolder PRS_output/$samplename.txt
 
 ## What does this app output?
-This app outputs one text file with three lines. 
+This app outputs a decompressed VCF file adn one text file with three lines. 
 Line 1 lists the genotypes, line2 lists the PRS and line 3 lists the risk and the decile.
-The result file is output to `/PRS_output`
+The outputs are saved to `/PRS_output`
 
 If skip = true no output is produced.
 
--- src/code.sh
+++ src/code.sh
@@ -3,33 +3,57 @@
 # The following line causes bash to exit at any point if there is any error
 # and to output each line as it is executed -- useful for debugging
 set -e -x -o pipefail
-if [ $skip == false ]; 
-	then
+if [ $skip == false ];
+	then		
 		#Grab inputs
 		dx-download-all-inputs --parallel
-		
-		# if vcf is gzipped decompress (removes .gz file)
-		if [[ $VCF_name =~ \.gz$ ]]; then
-			gunzip $VCF_path
-			# capture the vcf_name without the .gz (remove everything after the last full stop)
-			vcf_name=${VCF_name%.*}
-		else
-			# if not compressed can use the input filename
-			vcf_name=$VCF_name
-		fi
+		#Get reference genome
+		#dx cat $fasta_file | tar xf - # ~/hs37d5-fasta.tar -> ~/hs37d5.fa
+		dx cat project-ByfFPz00jy1fk6PjpZ95F27J:file-BxVGV9Q022qPQ5f2pbQYqbP4 | tar xf - # ~/hs37d5-fasta.tar -> ~/hs37d5.fa
+		echo "ZIPPED truth VCF unzipping."
+		#Unzip the vcf
+		gzip -d ${VCF_path}
+		#Remove the .gz suffix from truth_vcf filepath
+		vcf_path=$(echo ${VCF_path%.*})
+		#install tabix
+		apt-get install tabix
+		#Zip VCFs
+		bgzip $vcf_path
+		#Following gzipping, append .gz to vcf filepath variables
+		vcf_path=${vcf_path}.gz
+		#Index VCFs
+		tabix -p vcf ${vcf_path}
+		#download docker file
+		Docker_file_ID=project-ByfFPz00jy1fk6PjpZ95F27J:file-G55XqF00jy1QkJ174ZzZfzV5
+  		dx download ${Docker_file_ID}
 
+		Docker_image_file=$(dx describe ${Docker_file_ID} --name)
+  		Docker_image_name=$(tar xfO "${Docker_image_file}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
+		
 		# make output folder
 		mkdir -p ~/out/PRS_output/PRS_output
-		
-		# capture samplename from file name (remove everything after the first fullstop)
-		samplename=${VCF_name%%.*}
 
+		vcf_name=${VCF_name%.g.vcf*}
+
+		docker load < /home/dnanexus/"${Docker_image_file}"
+  		echo "Using docker image ${Docker_image_name}"
+  		docker run -v /home/dnanexus:/home/dnanexus --rm ${Docker_image_name} convert --gvcf2vcf -R $BEDfile_path -f /home/dnanexus/hs37d5.fa -Ov -o ~/out/PRS_output/PRS_output/${vcf_name}.vcf $vcf_path
+
+		#save samplename
+		samplename=$vcf_name
+
+		#Set up FH docker image
+		fh_docker_file_id=project-ByfFPz00jy1fk6PjpZ95F27J:file-G7p1PfQ0jy1bPBJP8kp0VGZp
+		dx download ${fh_docker_file_id}
+
+		fh_docker_image_file=$(dx describe ${fh_docker_file_id} --name)
+		fh_docker_image_name=$(tar xfO "${fh_docker_image_file}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
 		# load docker image
-		docker load  --input ~/fh_prs_v1.0.tgz
+		docker load < /home/dnanexus/"${fh_docker_image_file}"
 
 		# docker run. mount input directory as /sandbox, use the $vcf_name variable set above to create the docker path to mounted dir.
 		# Output to output folder named $samplename.txt
-		docker run -v /home/dnanexus/in/VCF:/sandbox fh_prs_v1.0:latest /sandbox/$vcf_name > ~/out/PRS_output/PRS_output/$samplename.txt
+		docker run -v /home/dnanexus:/home/dnanexus --rm ${fh_docker_image_name} ~/out/PRS_output/PRS_output/${vcf_name}.vcf > ~/out/PRS_output/PRS_output/$samplename.txt
 
 		# Send output back to DNAnexus project
 		mark-section "Upload output"
